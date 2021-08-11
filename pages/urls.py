306d7b594421a0744c82b62dd8c4014e74190d5d
invinciblecoder1 <adityakedia08@gmail.com>
2021-08-11 22:21:24 +0530

    Initial commit

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib import admin
+
+from . import views 
+
+urlpatterns =[
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    
+]
